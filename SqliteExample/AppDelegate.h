ba209379b0e12bf1ad33f8c53dc44b9492312639
Manju Sj <manjusj@Manjus-Mac.local>
2016-05-19 14:41:59 +0530

    Initial Commit

---
+++ SqliteExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SqliteExample
+//
+//  Created by Manju Sj on 5/19/16.
+//  Copyright (c) 2016 Manju Sj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
